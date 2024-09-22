9828335a4d9eb527405a79b9d6c41e4cd0cee22b
Noah <noahsragow@trygatsby.com>
2024-09-22 14:24:43 -0400

    temp

--- example.py
+++ example.py
@@ -15,9 +15,9 @@ sample_rate = 44100
 
 cosyvoice = CosyVoice('pretrained_models/CosyVoice-300M')
 # zero_shot usage, <|zh|><|en|><|jp|><|yue|><|ko|> for Chinese/English/Japanese/Cantonese/Korean
-prompt_speech_16k = load_wav('zero_shot_prompt.wav', 16000)
-for i, j in enumerate(cosyvoice.inference_zero_shot('收到好友从远方寄来的生日礼物，那份意外的惊喜与深深的祝福让我心中充满了甜蜜的快乐，笑容如花儿般绽放。', '希望你以后能够做的比我还好呦。', prompt_speech_16k, stream=False)):
-    torchaudio.save('zero_shot_{}.wav'.format(i), j['tts_speech'], 22050)
+# prompt_speech_16k = load_wav('zero_shot_prompt.wav', 16000)
+# for i, j in enumerate(cosyvoice.inference_zero_shot('收到好友从远方寄来的生日礼物，那份意外的惊喜与深深的祝福让我心中充满了甜蜜的快乐，笑容如花儿般绽放。', '希望你以后能够做的比我还好呦。', prompt_speech_16k, stream=False)):
+#     torchaudio.save('zero_shot_{}.wav'.format(i), j['tts_speech'], 22050)
 
 # cross_lingual usage
 # prompt_speech_16k = load_wav('cross_lingual_prompt.wav', 16000)
@@ -25,8 +25,8 @@ for i, j in enumerate(cosyvoice.inference_zero_shot('收到好友从远方寄来
 #     torchaudio.save('cross_lingual_{}.wav'.format(i), j['tts_speech'], 22050)
 
     # cross_lingual usage
-# prompt_speech_16k = load_wav('NoahMonolougingcopy.wav', sample_rate)
-# for i, j in enumerate(cosyvoice.inference_cross_lingual('<|ja|>こんにちは、今日は何をしていますか', prompt_speech_16k, stream=False)):
-#     torchaudio.save('cross_lingual_{}.wav'.format(i), j['tts_speech'], 22050)
+prompt_speech_16k = load_wav('NoahMonolougingcopy.wav', sample_rate)
+for i, j in enumerate(cosyvoice.inference_cross_lingual('<|ja|>こんにちは、今日は何をしていますか', prompt_speech_16k, stream=False)):
+    torchaudio.save('cross_lingual_{}.wav'.format(i), j['tts_speech'], 22050)
 # for i, j in enumerate(cosyvoice.inference_cross_lingual('<|zh|>你好，你今天在做什么', prompt_speech_16k, stream=False)):
 #     torchaudio.save('cross_lingual_{}.wav'.format(i), j['tts_speech'], 22050)
